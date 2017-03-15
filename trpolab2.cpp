85d7282bc790fc578a96380a8e7a23391eecd7c5
Misha Belashov <cabaka-babaka@mail.ru>
2017-03-16 04:44:13 +0800

    Vivod

--- trpolab2.cpp
+++ trpolab2.cpp
@@ -10,13 +10,13 @@ main(){
 	if (Srok<=365 && SummaVk>=10000){
 		if (SummaVk<=100000){
 			if (Srok>=0 && Srok <=30)
-			Rezult=SummaVk*0.9;
+			    Rezult=SummaVk*0.9;
 		    if (Srok>=31 && Srok <=120)
-		    Rezult=SummaVk*1.02;
+		        Rezult=SummaVk*1.02;
 		    if (Srok>=121 && Srok <=240)
-		    Rezult=SummaVk*1.06;
+		        Rezult=SummaVk*1.06;
 		    if (Srok>=241 && Srok <=365)
-		    Rezult=SummaVk*1.12;
+		        Rezult=SummaVk*1.12;
 		}
 	        else {
 	        		if (Srok>=0 && Srok <=30)
@@ -31,6 +31,7 @@ main(){
 	}
 	    else 
 	    printf ("Srok vklada ili Summa vklada vvedeni neverno");
+	printf ("Summa vklada na moment okonchaniya sroka: %d\n", Rezult);	
 	system("pause");
 	return 0;
 }
